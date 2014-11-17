423a4b50b12776d77b593231ad2f743efaec0070
Francisco Flores <francisco.flores@crowdint.com>
2014-11-17 17:08:37 -0600

    Fix updating total_on_hand after making an order

--- coffeeapp/MenuViewController_iPhone.m
+++ coffeeapp/MenuViewController_iPhone.m
@@ -168,7 +168,7 @@
                         //Check the stock and the quantity ordered
                         //Sustract the ordered quantity to total on hand. If more than 0, update the stock to a new temporally stock, in other way, the stock is set to 0
                         int productStock = [tmpProductObject total_on_hand] - [[dictTmpProduct objectForKey:@"TOTAL"] intValue];
-                        (productStock > 0)?[productObject setTotal_on_hand:productStock]:[tmpProductObject setTotal_on_hand:0];
+                        (productStock > 0)?[tmpProductObject setTotal_on_hand:productStock]:[tmpProductObject setTotal_on_hand:0];
                         NSLog(@"%d", [tmpProductObject total_on_hand]);
                         continue;
                     }
